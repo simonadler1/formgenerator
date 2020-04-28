243ad6dff9fc33223b4f335a45d89818bf35e902
adler.shimon@gmail.com <adler.shimon@gmail.com>
2020-04-28 00:52:28 -0400

    initial markup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <img class="profile-picture" alt="Profile Picture" src="./assets/images/profilecropped.png" />
+    </div>
+    <span class="seperator"></span>
+    <div class="project-container">
+      <div class="project">
+        <h2 class="project-heading">Project Name</h2>
+        <img src alt="Project Thumbnail" class="project-thumbnail" />
+        <h3 class="project-text">A short description of the project</h3>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <style>
+body {
+  background-color: #dbdbdb;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  border: 2px solid;
+  border-color: #f0a500;
+  /* background-color: #182f5e; */
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.seperator {
+  display: block;
+  border: 1px solid #cf7500;
+  width: 100%;
+}
+.profile-picture {
+  border-radius: 50%;
+  height: 35vh;
+}
+.project-container {
+  /* background-color: #d4d3d3; */
+  display: flex;
+  flex-wrap: wrap;
+}
+.project {
+  margin: 50px 0 50px 0;
+  width: 33%;
 }
 </style>
